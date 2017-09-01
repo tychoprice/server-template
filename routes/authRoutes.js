d45246356549c36d456a8f9d965e7e89287c8082
Tycho Price <tychoprice@gmail.com>
2017-08-31 22:30:18 -0700

    complete google oauth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,10 +5,9 @@ module.exports = app => {
     passport.authenticate('google', {scope: ['profile', 'email']})
   );
 
-app.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/');
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
   });
 };
