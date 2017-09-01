1571b09bda24c5d21a1614996b6d2028e62152c0
Tycho Price <tychoprice@gmail.com>
2017-08-31 21:39:42 -0700

    added User model

--- services/passport.js
+++ services/passport.js
@@ -1,17 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-passport.use
-  (new GoogleStrategy({
+const User = mongoose.model('users');
+
+passport.use(
+  new GoogleStrategy({
     clientID: keys.GOOGLE_CLIENT_ID,
     clientSecret: keys.GOOGLE_CLIENT_SECRET,
     callbackURL: '/auth/google/callback'
   },
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
+      User.findOne({googleId: profile.id}).then((existingUser) => {
+        if (existingUser) {
+          //already have record in db
+          done(null, existingUser);
+        } else {
+          //make new record
+          new User({googleId: profile.id})
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
