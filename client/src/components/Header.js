6a03ec0cd205812986f57d5f06877a3bb3708795
Tycho Price <tychoprice@gmail.com>
2017-09-04 17:08:41 -0700

    updated auth

---
+++ client/src/components/Header.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class Header extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            Tycho
+          </a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+function mapStateToProps({auth}) {
+  return {auth};
+}
+
+export default connect(mapStateToProps) (Header);
--- services/passport.js
+++ services/passport.js
@@ -22,18 +22,16 @@ passport.use(
     callbackURL: '/auth/google/callback',
     proxy: true
   },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({googleId: profile.id}).then((existingUser) => {
-        if (existingUser) {
-          //already have record in db
-          done(null, existingUser);
-        } else {
-          //make new record
-          new User({googleId: profile.id})
-            .save()
-            .then(user => done(null, user));
-        }
-      });
-    }
-  )
-);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({googleId: profile.id});
+
+      if (existingUser) {
+      //already have record in db
+        return done(null, existingUser);
+      }
+      //make new record
+      const user = await new User({googleId: profile.id}).save();
+      done(null, user);
+      }
+    )
+  );
