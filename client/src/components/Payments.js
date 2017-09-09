9fff13d28ae65c16552a88ddc06cabe6f2a32768
Tycho Price <tychoprice@gmail.com>
2017-09-09 16:10:39 -0700

    add body-parser

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="SurveyDonkey"
+        description="$5 for 5 credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
---
+++ routes/billingRoutes.js
@@ -0,0 +1,8 @@
+const stripe = require('stripe');
+const keys = require('../config/keys')(keys.stripeSecretKey);
+
+module.exports = app => {
+  app.post('/api/stripe', (res, res) => {
+
+  });
+};
